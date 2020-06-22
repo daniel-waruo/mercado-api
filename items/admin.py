3d93907d7240a895a72beaf1c151b8efb2ad6347
Daniel Waruo <waruodaniel@gmail.com>
2020-06-22 18:24:57 +0300

    added models to django admin

--- items/admin.py
+++ items/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Item, Category, Brand
 
 # Register your models here.
+admin.site.register(Item)
+admin.site.register(Category)
+admin.site.register(Brand)
