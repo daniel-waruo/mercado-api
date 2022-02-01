d1edea1d68fbec04256e82379b01e7cbd2fec1fa
Daniel Waruo <waruodaniel@gmail.com>
2022-02-01 07:14:47 +0300

    fix: remove authenctication on the image urls

--- root/settings.py
+++ root/settings.py
@@ -155,6 +155,7 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_QUERYSTRING_AUTH = False
 
 # set screen urls path
 SCREEN_URLS_PATH = 'gas_screens.screen_urls'
