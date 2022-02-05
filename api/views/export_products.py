7dac2dff6d50a3b45902a746fa289fc482a8d4f3
Daniel Waruo <waruodaniel@gmail.com>
2022-02-05 07:41:37 +0300

    feature: upload products batch

--- api/views/export_products.py
+++ api/views/export_products.py
@@ -4,22 +4,18 @@ from django.core.handlers.wsgi import WSGIRequest
 from django.http import JsonResponse
 
 from products.models import Product
-from products.utils import update_facebook_catalog
+from products.utils import update_facebook_catalog, update_facebook_batch
 
 
 def export_products(request: WSGIRequest):
     if request.method == 'POST':
         products = Product.objects.all()
         data = json.loads(request.body)
-        for product in products:
-            try:
-                update_facebook_catalog(
-                    product,
-                    "886770702008655",
-                    data.get('accessToken')
-                )
-            except (ValueError, AttributeError):
-                pass
+        update_facebook_batch(
+            products,
+            "886770702008655",
+            data.get('accessToken')
+        )
         return JsonResponse({
             'message': 'Successfully Uploaded Products'
         })
--- products/utils.py
+++ products/utils.py
@@ -12,7 +12,6 @@ def update_facebook_catalog(product: Product, catalog_id, access_token, operatio
                 'method': operation,
                 'retailer_id': product.sku,
                 'data': body or {
-
                     'availability': 'in stock',
                     'brand': product.brand.name,
                     'category': product.category.name,
@@ -28,6 +27,42 @@ def update_facebook_catalog(product: Product, catalog_id, access_token, operatio
                 }
             }
         ]
+    }
+    response = requests.request(
+        "POST",
+        url,
+        json=payload
+    )
+    print(response.text)
+
+
+def update_facebook_batch(products, catalog_id, access_token, operation='UPDATE'):
+    url = f"https://graph.facebook.com/v12.0/{catalog_id}/batch"
+    facebook_requests = []
+    for product in products:
+        facebook_requests.append({
+            {
+                'method': operation,
+                'retailer_id': product.sku,
+                'data': {
+                    'availability': 'in stock',
+                    'brand': product.brand.name,
+                    'category': product.category.name,
+                    'description': product.description or product.name,
+                    'image_url': product.image.url,
+                    'name': product.name,
+                    'price': int(float(product.price) * 100),
+                    'currency': 'KES',
+                    'condition': 'new',
+                    'inventory': product.in_stock,
+                    'url': "https://makinika.com",
+                    'retailer_product_group_id': product.sku,
+                }
+            }
+        })
+    payload = {
+        'access_token': access_token,
+        'requests': facebook_requests
     }
     response = requests.request(
         "POST",
